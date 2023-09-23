5283a737831acd2c4ac053313a56a01eb3a8689c
Philcob Suzuki Josol <pjguitar15@gmail.com>
2023-09-24 00:27:32 +0800

    changed font to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Oswald } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
-const poppins = Oswald({ weight: '400', subsets: ['latin'] })
+const poppins = Poppins({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Newsletter Email Validation Project',
